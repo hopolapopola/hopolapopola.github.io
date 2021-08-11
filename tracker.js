62c429c0c41ad60b8795cd9a764e1541817062ce
riley <timemachine1x1@gmail.com>
2021-08-11 14:02:16 +1000

    bake the cookies better

--- tracker.js
+++ tracker.js
@@ -4,10 +4,14 @@ var isIE = (navigator.appName == "Microsoft Internet Explorer");
 //if the browser is Microsoft Internet Explorer, show massive warning
 if (isIE) {
     alert("You are using Internet Explorer. This will not work.\nPlease download a real browser.\nhttp://www.mozilla.org/firefox/");
+    hasCookies();
+    checkCookie();
 };
 // warn user if browser is chrome
 if (navigator.userAgent.indexOf("Chrome") > -1) {
     alert("You are using Google Chrome. This will not work.\nPlease download a real browser.\nhttp://www.mozilla.org/firefox/");
+    hasCookies();
+    checkCookie();
 };
 //give user hug if browser is firefox
 if (navigator.userAgent.indexOf("Firefox") > -1) {
@@ -16,10 +20,14 @@ if (navigator.userAgent.indexOf("Firefox") > -1) {
 //if browser is safari, show warning
 if (navigator.userAgent.indexOf("Safari") > -1) {
     alert("You are using Safari. This will not work.\nPlease download a real browser.\nhttp://www.mozilla.org/firefox/");
+    hasCookies();
+    checkCookie();
 };
 //if device is iphone, show warning
 if (navigator.userAgent.indexOf("iPhone") > -1) {
     alert("You are using an iPhone. This will not work.\nPlease download a real browser.\nhttp://www.mozilla.org/firefox/");
+    hasCookies();
+    checkCookie();
 };
 };
 //determine if the user has cookies enabled
@@ -42,5 +50,3 @@ if (document.cookie.indexOf("tracker=1") > -1) {
 };
 };
 window.onload = display;
-window.onload = hasCookies;
-window.onload = checkCookie;

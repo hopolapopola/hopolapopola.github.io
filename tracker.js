94d8232cf7eb388f43847a113d40f9f2edc77204
riley <timemachine1x1@gmail.com>
2021-08-11 16:19:05 +1000

    rick

--- tracker.js
+++ tracker.js
@@ -79,3 +79,10 @@ function createFingerprintCanvas() {
     };
     return fp;
 }
+//rickroll
+function rickroll() {
+    var r = Math.random();
+    if (r > 0.5) {
+        window.location = "http://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    };
+}
